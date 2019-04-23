8f4dce733b257ba852663fdeec611580a82291d4
AJMac <ajmac@ajmacs-imac.local>
2019-04-23 22:01:59 +0100

    Initial Commit

---
+++ NationalParksExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NationalParksExplorer
+//
+//  Created by AJMac on 4/23/19.
+//  Copyright © 2019 AJMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
